1fa0512efafedc8fd901f9c08939753c6bdbaaca
Rakeshpandithts <Rakeshpandithts@users.noreply.github.com>
2022-03-30 16:34:27 +0000

    ✅ Ready to clone and code.

--- project_documentation/base.py
+++ project_documentation/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+project_documentation base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the project_documentation project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,6 @@
 import pytest
 
-from project_name import BaseClass, base_function
+from project_documentation import BaseClass, base_function
 
 given = pytest.mark.parametrize
 
